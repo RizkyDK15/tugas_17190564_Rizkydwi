b30a03ab6e61df95a7dd8f959d998139825c69d3
RizkyDK15 <101790971+RizkyDK15@users.noreply.github.com>
2022-04-29 13:16:09 +0700

    Create contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h> Perkenalkan </h> <br>";
+        echo " Nama Saya Rizky Dwi Kuncoro<br>
+        Saya lahir di tangerang 15 MEI 2001 <br>
+        Saya tinggal di Kota Tangerang";
+    }
+} 
